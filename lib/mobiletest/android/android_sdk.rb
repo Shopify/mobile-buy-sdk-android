c19cba5b30cae4753762e39832b764576c990471
Julian Nadeau <julian@jnadeau.ca>
2016-02-24 16:46:24 -0500

    Bump gems, change config var

--- lib/mobiletest/android/android_sdk.rb
+++ lib/mobiletest/android/android_sdk.rb
@@ -4,13 +4,13 @@ class AndroidSDK
 
   def initialize(config={})
     @config = config
-    @config[:android_versions] ||= ["android-21", "sys-img-x86-android-21"]
+    @config[:sdk_packages] ||= ["android-21", "sys-img-x86-android-21"]
   end
 
   def update!
     puts "Updating SDK"
-    @config[:android_versions].each do |version|
-      puts "Updating #{version}"
+    @config[:sdk_packages].each do |package|
+      puts "Updating #{package}"
       system "echo y | android --silent update sdk --no-ui --all --filter \"#{version}\" &>/dev/null"
     end
   end
--- mobiletest.gemspec
+++ mobiletest.gemspec
@@ -28,6 +28,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "ADB"
+  spec.add_dependency "fastlane"
 
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
