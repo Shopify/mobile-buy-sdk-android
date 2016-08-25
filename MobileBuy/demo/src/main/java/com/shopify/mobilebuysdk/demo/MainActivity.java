cebdb73e3e7b460ba72843d08dff490874185b1a
Henry Tao <hi@henrytao.me>
2016-08-25 21:31:21 +0700

    Init demo project

--- MobileBuy/demo/src/main/java/com/shopify/mobilebuysdk/demo/MainActivity.java
+++ MobileBuy/demo/src/main/java/com/shopify/mobilebuysdk/demo/MainActivity.java
@@ -23,4 +23,16 @@
  *
  */
 
-include ":sample", ":buy", ":demo"
+package com.shopify.mobilebuysdk.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
