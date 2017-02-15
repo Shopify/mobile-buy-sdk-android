b2728f19c45666fde8981e1759f76a5e47b5909f
ivan.savytskyi <ivan.savytskyi@shopify.com>
2017-02-15 14:43:52 -0500

    cleanup

--- MobileBuy/sample/src/main/java/com/shopify/sample/application/SampleApplication.java
+++ MobileBuy/sample/src/main/java/com/shopify/sample/application/SampleApplication.java
@@ -24,20 +24,11 @@
 
 package com.shopify.sample.application;
 
-import android.app.Activity;
 import android.app.Application;
-import android.content.Intent;
-import android.net.Uri;
 import android.text.TextUtils;
-import android.widget.Toast;
 
-import com.google.android.gms.wallet.FullWallet;
-import com.google.android.gms.wallet.MaskedWallet;
 import com.google.android.gms.wallet.WalletConstants;
 import com.shopify.sample.BuildConfig;
-import com.shopify.sample.R;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -46,45 +37,48 @@ import okhttp3.logging.HttpLoggingInterceptor;
  */
 public class SampleApplication extends Application {
 
-    private static final String SHOP_PROPERTIES_INSTRUCTION =
-        "\n\tAdd your shop credentials to a shop.properties file in the main app folder (e.g. 'app/shop.properties'). Include these keys:\n" +
-            "\t\tSHOP_DOMAIN=<myshop>.myshopify.com\n" +
-            "\t\tAPI_KEY=0123456789abcdefghijklmnopqrstuvw\n";
+  private static final String SHOP_PROPERTIES_INSTRUCTION =
+    "\n\tAdd your shop credentials to a shop.properties file in the main app folder (e.g. 'app/shop.properties')."
+      + "Include these keys:\n" + "\t\tSHOP_DOMAIN=<myshop>.myshopify.com\n"
+      + "\t\tAPI_KEY=0123456789abcdefghijklmnopqrstuvw\n";
 
-    private static SampleApplication instance;
+  private static SampleApplication instance;
 
-    public static final String ANDROID_PAY_FLOW = "com.shopify.sample.androidpayflow";
+  public static final String ANDROID_PAY_FLOW = "com.shopify.sample.androidpayflow";
 
-    // Use ENVIRONMENT_TEST for testing
-    public static final int WALLET_ENVIRONMENT = WalletConstants.ENVIRONMENT_TEST;
+  // Use ENVIRONMENT_TEST for testing
+  public static final int WALLET_ENVIRONMENT = WalletConstants.ENVIRONMENT_TEST;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+  @Override
+  public void onCreate() {
+    super.onCreate();
 
-        instance = this;
+    instance = this;
 
-        initializeBuyClient();
+    initializeBuyClient();
+  }
+
+  private void initializeBuyClient() {
+    String shopUrl = BuildConfig.SHOP_DOMAIN;
+    if (TextUtils.isEmpty(shopUrl)) {
+      throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must add 'SHOP_DOMAIN' entry in "
+        + "app/shop.properties, in the form '<myshop>.myshopify.com'");
     }
 
-    private void initializeBuyClient() {
-        String shopUrl = BuildConfig.SHOP_DOMAIN;
-        if (TextUtils.isEmpty(shopUrl)) {
-            throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must add 'SHOP_DOMAIN' entry in app/shop.properties, in the form '<myshop>.myshopify.com'");
-        }
-
-        String shopifyApiKey = BuildConfig.API_KEY;
-        if (TextUtils.isEmpty(shopifyApiKey)) {
-            throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must populate the 'API_KEY' entry in app/shop.properties");
-        }
-
-        String shopifyAppId = BuildConfig.APP_ID;
-        if (TextUtils.isEmpty(shopifyAppId)) {
-            throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must populate the 'APP_ID' entry in app/shop.properties");
-        }
-
-        String applicationName = getPackageName();
-
-        final HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(BuildConfig.OKHTTP_LOG_LEVEL);
+    String shopifyApiKey = BuildConfig.API_KEY;
+    if (TextUtils.isEmpty(shopifyApiKey)) {
+      throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must populate the 'API_KEY' entry in "
+        + "app/shop.properties");
     }
+
+    String shopifyAppId = BuildConfig.APP_ID;
+    if (TextUtils.isEmpty(shopifyAppId)) {
+      throw new IllegalArgumentException(SHOP_PROPERTIES_INSTRUCTION + "You must populate the 'APP_ID' entry in "
+        + "app/shop.properties");
+    }
+
+    String applicationName = getPackageName();
+
+    final HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(BuildConfig.OKHTTP_LOG_LEVEL);
+  }
 }
